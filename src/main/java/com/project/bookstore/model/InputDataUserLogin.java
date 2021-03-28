cad4697a7008165a8169b4f996457bc6d822888e
B-9991 <43391914+B-9991@users.noreply.github.com>
2021-03-28 03:33:38 +0300

    Create InputDataUserLogin.java

---
+++ src/main/java/com/project/bookstore/model/InputDataUserLogin.java
@@ -0,0 +1,26 @@
+package com.project.bookstore.model;
+
+public class InputDataUserLogin {
+	private String password;
+	private String email;
+
+	public InputDataUserLogin() {
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
