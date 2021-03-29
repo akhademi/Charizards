78447898795a46f0d57d0fa85e39090425257d2e
Rachel <rachelsmtam@yahoo.com>
2021-03-29 19:46:07 -0400

    Added necessary classes to be completed in the repository package

--- src/main/java/com/project/bookstore/repository/AdminRepo.java
+++ src/main/java/com/project/bookstore/repository/AdminRepo.java
@@ -20,74 +20,74 @@ import java.util.List;
 @Repository
 public class AdminRepo {
 
-  Logger log = LoggerFactory.getLogger(UserCtrl.class);
+	Logger log = LoggerFactory.getLogger(UserCtrl.class);
 
-  @Autowired
-  private EntityManager entityManager;
-  private Session getSession(){
-    return entityManager.unwrap(Session.class);
-  }
+	@Autowired
+	private EntityManager entityManager;
+	private Session getSession(){
+		return entityManager.unwrap(Session.class);
+	}
 
-  @Transactional
-  public List<BooksSoldModel> returnBooksSold(){
-    try{
-      Session session = getSession();
-      Query<?> query = session.createNativeQuery("with summed as " +
-              "    (select od.bid as bid, sum(od.quantity) as quantity from ORDER_DETAIL od " +
-              "    join BOOK B on od.BID = B.BID" +
-              "    left join ORDER O on O.order_id = od.order_id" +
-              "    where O.status = 1" +
-              "    group by od.bid) " +
-              "select S.bid, B.title, B.price, S.quantity from summed S " +
-              "join BOOK B on S.bid = B.bid ");
-      List<Object[]> itemList = (List<Object[]>)query.getResultList();
+	@Transactional
+	public List<BooksSoldModel> returnBooksSold() {
+		try{
+			Session session = getSession();
+			Query<?> query = session.createNativeQuery("with summed as " +
+					"    (select od.bid as bid, sum(od.quantity) as quantity from ORDER_DETAIL od " +
+					"    join BOOK B on od.BID = B.BID" +
+					"    left join ORDER O on O.order_id = od.order_id" +
+					"    where O.status = 1" +
+					"    group by od.bid) " +
+					"select S.bid, B.title, B.price, S.quantity from summed S " +
+					"join BOOK B on S.bid = B.bid ");
+			List<Object[]> itemList = (List<Object[]>)query.getResultList();
 
-      List<BooksSoldModel> books = new ArrayList<>();
-      for(Object[] bookData: itemList){
-    	BooksSoldModel item = new BooksSoldModel();
-        item.setBid((int)bookData[0]);
-        item.setTitle(String.valueOf(bookData[1]));
-        item.setPrice(Util.roundDouble((Double) bookData[2]));
-        BigInteger bg = new BigInteger(String.valueOf(bookData[3]));
-        item.setQuantity(bg.intValue());
-        books.add(item);
-      }
-      return books;
-    } catch (Exception e){
-      log.error(e.getMessage(), e);
-      return null;
-    }
-  }
+			List<BooksSoldModel> books = new ArrayList<>();
+			for(Object[] bookData: itemList){
+				BooksSoldModel item = new BooksSoldModel();
+				item.setBid((int)bookData[0]);
+				item.setTitle(String.valueOf(bookData[1]));
+				item.setPrice(Util.roundDouble((Double) bookData[2]));
+				BigInteger bg = new BigInteger(String.valueOf(bookData[3]));
+				item.setQuantity(bg.intValue());
+				books.add(item);
+			}
+			return books;
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+			return null;
+		}
+	}
 
-  @Transactional
-  public List<BooksSoldModel> topSoldBooks(){
-    try{
-      Session session = getSession();
-      Query<?> query = session.createNativeQuery("with summed as " +
-              "    (select od.bid as bid, sum(od.quantity) as quantity from ORDER_DETAIL od " +
-              "    join BOOK B on od.BID = B.BID" +
-              "    left join ORDER O on O.order_id = od.order_id" +
-              "    where O.status = 1" +
-              "    group by od.bid) " +
-              "select S.bid, B.title, S.quantity from summed S " +
-              "join BOOK B on S.bid = B.bid " +
-              "order by S.quantity desc limit 10");
-      List<Object[]> itemList = (List<Object[]>)query.getResultList();
+	@Transactional
+	public List<BooksSoldModel> topSoldBooks() {
+		try{
+			Session session = getSession();
+			Query<?> query = session.createNativeQuery("with summed as " +
+					"    (select od.bid as bid, sum(od.quantity) as quantity from ORDER_DETAIL od " +
+					"    join BOOK B on od.BID = B.BID" +
+					"    left join ORDER O on O.order_id = od.order_id" +
+					"    where O.status = 1" +
+					"    group by od.bid) " +
+					"select S.bid, B.title, S.quantity from summed S " +
+					"join BOOK B on S.bid = B.bid " +
+					"order by S.quantity desc limit 10");
+			List<Object[]> itemList = (List<Object[]>)query.getResultList();
 
-      List<BooksSoldModel> books = new ArrayList<>();
-      for(Object[] bookData: itemList){
-    	BooksSoldModel item = new BooksSoldModel();
-        item.setBid((int)bookData[0]);
-        item.setTitle(String.valueOf(bookData[1]));
-        BigInteger bg = new BigInteger(String.valueOf(bookData[2]));
-        item.setQuantity(bg.intValue());
-        books.add(item);
-      }
-      return books;
-    } catch (Exception e){
-      log.error(e.getMessage(), e);
-      return null;
-    }
-  }
+			List<BooksSoldModel> books = new ArrayList<>();
+			for(Object[] bookData: itemList){
+				BooksSoldModel item = new BooksSoldModel();
+				item.setBid((int)bookData[0]);
+				item.setTitle(String.valueOf(bookData[1]));
+				BigInteger bg = new BigInteger(String.valueOf(bookData[2]));
+				item.setQuantity(bg.intValue());
+				books.add(item);
+			}
+			return books;
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+			return null;
+		}
+	}
 
 }
