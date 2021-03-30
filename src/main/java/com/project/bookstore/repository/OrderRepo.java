0513aa6f7982a62fe3068a86788e8b6ffa1dc85a
Rachel <rachelsmtam@yahoo.com>
2021-03-30 03:17:55 -0400

    Added methods to be completed in OrderRepo and ReviewRepo classes

--- src/main/java/com/project/bookstore/repository/OrderRepo.java
+++ src/main/java/com/project/bookstore/repository/OrderRepo.java
@@ -1,5 +1,52 @@
 package com.project.bookstore.repository;
 
-public class OrderRepo {
+import com.project.bookstore.model.*;
 
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class OrderRepo {
+	
+	// return cart of specified user ID
+	public EntityOrder findCartByID(String userID) {
+		return null;
+	}
+	
+	// add a new cart and if successful, return cart
+	public EntityOrder insertNewCart(String userID) {
+		return null;
+	}
+	
+	// return items in specified cart
+	public List<CartItem> returnCartData(int orderID) {
+		return null;
+	}
+	
+	// return order of specified book ID (BID)
+	public List<InputDataOrderProcessed> returnOrderByBID(int bid) {
+		return null;
+	}
+	
+	// add items to cart
+	public int addItems(int orderID, List<EntityOrderDetail> items) {
+		return 0;
+	}
+		
+	// remove item from cart
+	public int removeItem(int orderID, int bid) {
+		return 0;
+	}
+	
+	// increase quantity of book in cart if same book ordered again
+	public int incSubmitAttempts(String userID, int orderID) {
+		return 0;
+	}
+	
+	// submit order
+	public int submitOrder(String userId, int orderId) {
+		return 0;
+	}
+	
 }
--- src/main/java/com/project/bookstore/repository/ReviewRepo.java
+++ src/main/java/com/project/bookstore/repository/ReviewRepo.java
@@ -1,5 +1,21 @@
 package com.project.bookstore.repository;
 
-public class ReviewRepo {
+import com.project.bookstore.model.*;
 
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ReviewRepo {
+	
+	// return reviews of specified book
+	public List<EntityReview> getReviews(int bid) {
+		return null;
+	}
+	
+	// add review for specified book
+	public int addReview(int bid, String userID, double rating, String message) {
+		return 0;
+	}
 }
