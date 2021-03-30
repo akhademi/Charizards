aedd47cc6112e1e47ddfbb1d02d4ed6e5f987ae2
Rachel <rachelsmtam@yahoo.com>
2021-03-30 15:35:01 -0400

    Added methods to complete in UserRepo class

--- src/main/java/com/project/bookstore/repository/UserRepo.java
+++ src/main/java/com/project/bookstore/repository/UserRepo.java
@@ -1,5 +1,34 @@
 package com.project.bookstore.repository;
 
+import com.project.bookstore.model.*;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 public class UserRepo {
 
+	public int signupUser(EntityUser user) {
+		return 0;
+	}
+	
+	public EntityUser getUser(String email) {
+		return null;
+	}
+	
+	public EntityUser getAdminByUserID(String userID) {
+		return null;
+	}
+	
+	public int addAddress(InputDataAddress addressInfo) {
+		return 0;
+	}
+	
+	public EntityAddressModel getAddress(String userID) {
+		return null;
+	}
+	
+	public boolean emailExist(String email) {
+		return false;
+	}
 }
