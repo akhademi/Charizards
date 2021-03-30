ad0a1820143859ebc814a7af6484e60fb2181422
Rachel <rachelsmtam@yahoo.com>
2021-03-30 01:54:28 -0400

    Completed BookRepo class

--- src/main/java/com/project/bookstore/repository/BookRepo.java
+++ src/main/java/com/project/bookstore/repository/BookRepo.java
@@ -1,5 +1,38 @@
 package com.project.bookstore.repository;
 
-public class BookRepo {
+import com.project.bookstore.model.EntityBookModel;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+// book repository uses the CRUD repository
+public interface BookRepo extends CrudRepository<EntityBookModel, Integer> {
+
+	// CRUD repository allows you query creation by looking for the prefixes: 
+	// find...By, read...By, get...By
+	
+	// these methods will be called from the BookCtrl and BookService class
+	
+    // method used to get 10 books at a time
+    @Query(value = "SELECT * FROM BOOK LIMIT 10 OFFSET ?1", nativeQuery = true)
+    List<EntityBookModel> findAllBook(Integer offset);
+    
+    // method used to search for book by title
+    @Query(value = "SELECT * FROM book WHERE UPPER(title) LIKE '%' || UPPER(?1) || '%' ORDER BY CATEGORY", nativeQuery = true)
+    List<EntityBookModel> searchBooksByTitle(String title);
+
+    // method used to get 10 books in a category
+    @Query(value = "SELECT * FROM book WHERE category = ?1 LIMIT 10 OFFSET ?2", nativeQuery = true)
+    List<EntityBookModel> getBooksByCategory(String category,Integer pageno);
+
+    // method used to get all categories
+    @Query(value = "SELECT CATEGORY FROM book GROUP BY CATEGORY", nativeQuery = true)
+    List<String> findAllCategory();
+
+    // method used to select a book by BID (book ID)
+    @Query(value = "SELECT * FROM book WHERE bid = :bid", nativeQuery = true)
+    EntityBookModel findBookEntityByBid(int bid);
 
 }
